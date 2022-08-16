1ce5482dc65a151b3259bb01895b6a05b48176b4
Matheus Bezerra <mabezerra2@gmail.com>
2022-08-16 17:46:42 -0400

    test: componente favoritePokemons
    criando teste de 100% de cobertura no favoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('Verefique se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonFound = screen.getByText(/No favorite pokemon found/i);
+    expect(pokemonFound).toBeInTheDocument();
+  });
+});
