b0bf55d3c3e529fd7720a2b9aaf660db832edf2a
Matheus Bezerra <mabezerra2@gmail.com>
2022-08-16 18:09:55 -0400

    test: componente NotFound
    Criando cobertura de teste 100% no componente NotFound.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('teste componente NotFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested', () => {
+    renderWithRouter(<NotFound />);
+    const NotFoundVar = screen.getByText(/Page requested not found/i);
+    expect(NotFoundVar).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imagem = screen.getByAltText(/Pikachu crying/i);
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
